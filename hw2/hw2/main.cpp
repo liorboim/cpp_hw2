5ecaf4eca2826b7a71addd2524562a5bad7e4c1b
liorboim <liorboim@gmail.com>
2015-04-24 10:53:20 +0300

    xcode start

---
+++ hw2/hw2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw2
+//
+//  Created by lior bomwurzel on 4/24/15.
+//  Copyright (c) 2015 lior bomwurzel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
